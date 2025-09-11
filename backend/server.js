3923292ab9983c40269b58a5118aa39441d93699
AnmarSammour <anmarsammour2000@gmail.com>
2025-09-11 05:00:33 +0300

    feat: Configure CORS for backend

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
 const path = require("path");
-
+const cors = require('cors');
 const app = express();
+app.use(cors()); 
 const PORT = 3000;
 
 // --------------------------- Middlewares ---------------------------
